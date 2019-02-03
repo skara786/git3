af22b4422f668419cf291acc68ed6c88ecc362bd
Sam Kara <skara786@gmail.com>
2019-02-03 15:48:47 -0600

    first commit

---
+++ src/git3/git3C.java
@@ -0,0 +1,11 @@
+package git3;
+
+public class git3C {
+
+	public void skprint() {
+		System.out.println("hello");
+	}
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
